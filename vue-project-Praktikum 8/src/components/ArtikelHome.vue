0cedea7bd99bb2323a8fb54c288d8dd2dbbb8ae6
Hyadawild <kudosinichi9290@gmail.com>
2024-05-30 17:13:44 +0700

    new file:   Upload file

---
+++ vue-project-Praktikum 8/src/components/ArtikelHome.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+<template>
+<div>
+    
+    <img src="../assets/anime1.gif"/>
+    <h1>
+        ini adalah komponen home
+    </h1>
+    <p> ini adalah artikel</p>
+    <p> ini adalah artikel</p>
+    <p> ini adalah artikel</p>
+    <p> ini adalah artikel</p>
+
+</div>
+</template>
---
+++ vue-project-Praktikum 8/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+//import TheWelcome from '../components/TheWelcome.vue'
+import ArtikelHome from '../components/ArtikelHome.vue'
+</script>
+
+<template>
+  <main>
+    <ArtikelHome/>
+    
+    
+  </main>
+</template>
